2319e0398ca2caab97d161c04604c02e70774949
Breno Reis de Oliveria <119299485+AldrichGE@users.noreply.github.com>
2024-08-15 23:07:05 -0300

    Interface 15/08
    +my-reserve card
    +service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NewRoomComponent } from './new-room/new-room.component';
 import { NewServiceComponent } from './new-service/new-service.component';
 import { ReserveFormComponent } from './reserve-form/reserve-form.component';
+import { provideHttpClient } from '@angular/common/http';
 
 
 @NgModule({
@@ -29,10 +31,12 @@ import { ReserveFormComponent } from './reserve-form/reserve-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
--- src/app/new-room/new-room.component.ts
+++ src/app/new-room/new-room.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './new-room.component.css'
 })
 export class NewRoomComponent {
-
 }
